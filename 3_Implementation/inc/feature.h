52cba211e761fb07ca8a5523d9765c9b7901827c
Gokul0808 <94180547+Gokul0808@users.noreply.github.com>
2021-11-23 22:55:56 +0530

    Update feature.h

--- 3_Implementation/inc/feature.h
+++ 3_Implementation/inc/feature.h
@@ -1,1 +1,26 @@
+/**
+ * @file feature.h
+ * @author Gokul0808
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+ 
+ #ifndef FEATURE_H_
+ #define FEATURE_H_
+ 
+ 
+ int bfrate();
+ int lunchrate();
+ int dinner_rate();
+ int choice();
+ int quantity();
+ int purchased();
 
+
+
+#define MACRO -99
+#endif
