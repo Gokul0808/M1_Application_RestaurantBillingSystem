955599234017c818fd86afc99ede4755f873097e
Gokul0808 <94180547+Gokul0808@users.noreply.github.com>
2021-11-24 17:02:46 +0530

    Update test_functions.c

--- 3_Implementation/test/test_functions.c
+++ 3_Implementation/test/test_functions.c
@@ -1,50 +1,91 @@
 #include "unity.h"
 
 #include "header.h"
-#define PROJECT_NAME "RestaurantBillingSystem"
 
-void bfast(char bfmenu[][100]);
-void lunch( char lunchmenu[][100]);
-void dinner(char dinnermenu[][100]);
-void main_menu();
-void display();
-void lunch_menucard();
-void dinner_menu();
-void display_bf_bill(char bfmenu[][100]);
-void display_lunch_bill(char lunchmenu[][100]);
-void display_dinner_bill(char dinnermenu[][100]);
-void display_bfmenu();
-void repeatbf(char bfmenu[][100]);
-void repeatlunch(char lunchmenu[][100]);
-void repeatdinner(char dinnermenu[][100]);
-void display_total_words();
+void test_bfast(void);
+void test_lunch(void);
+void test_dinner(void);
+void test_main_menu(void);
+void test_display(void);
+void test_lunch_menucard(void);
+void test_dinner_menu(void);
+void test_display_bf_bill(void);
+void test_display_lunch_bill(void);
+void test_display_dinner_bill(void);
+void test_display_bfmenu(void);
+void test_repeatbf(void);
+void test_repeatlunch(void);
+void test_repeatdinner(void);
+void test_display_total_words(void);
 
-void setUp()
-{
-}
-void tearDown()
-{
-}
+void setUp(){}
+/**
+ * @brief Required by the unity test framework
+ * 
+ */
+void tearDown(){}
 
-void test_login(void)
-{
-    char username[10]="tanuja", userpwd[10]="123";
-    //TEST_ASSERT_EQUAL_STRING(0,("tanuja","123"));
-    TEST_ASSERT_EQUAL_STRING("tanuja",username);
-    TEST_ASSERT_EQUAL_STRING("123",userpwd);
-    
-}
-void test_change_prize(void)
-{
-    int prize1=700,prize2=900;
-    TEST_ASSERT_EQUAL(700,change_prize(prize1));
-    TEST_ASSERT_EQUAL(900,change_prize(prize2));
-}
-
-int main(void)
+/**
+ * @brief Start of the application test
+ * 
+ * @return int 
+ */
+int main()
 {
   UNITY_BEGIN();
-  RUN_TEST(test_login);
-  RUN_TEST(test_change_prize);
+    
+  RUN_TEST(test_bfast);
+  RUN_TEST(test_lunch);
+  RUN_TEST(test_dinner);
+  RUN_TEST(test_main_menu);
+  RUN_TEST(test_display);
+  RUN_TEST(test_lunch_menucard);
+  RUN_TEST(test_dinner_menu);
+  RUN_TEST(test_display_bf_bill);
+  RUN_TEST(test_display_lunch_bill);
+  RUN_TEST(test_display_dinner_bill);
+  RUN_TEST(test_display_bfmenu);
+  RUN_TEST(test_repeatbfast);
+  RUN_TEST(test_repeatlunch);
+  RUN_TEST(test_repeatdinner);
+  RUN_TEST(test_display_total_words);
+  
   return UNITY_END();
+}
+
+void test_bfast(void) {
+  TEST_ASSERT_EQUAL(4, bfast(100, 1000, 2));
+}
+
+void test_discountcalculator(void) {
+  TEST_ASSERT_EQUAL(4, discountcalculator(100, 2));
+}
+
+void test_ppf(void) {
+  TEST_ASSERT_EQUAL(4, ppf(100, 2));
+  
+}
+
+void test_emi(void) {
+  TEST_ASSERT_EQUAL(0, emi(100, 2, 2));
+  
+}
+
+void test_rd(void) {
+  TEST_ASSERT_EQUAL(4, rd(100, 2, 2));
+  
+}
+
+void test_fd(void) {
+  TEST_ASSERT_EQUAL(4, fd(100, 2, 2));
+  
+}
+
+void test_sip(void) {
+  TEST_ASSERT_EQUAL(4, sip(100, 2, 2));
+  
+}
+
+void test_lumpsum(void) {
+  TEST_ASSERT_EQUAL(4, lumpsum(100, 2, 2));
 }
